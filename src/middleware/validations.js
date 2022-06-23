0c24a73f17f5b007fbacff809213c5ef12d8b9ac
KerliS9 <kerlischroeder9@gmail.com>
2022-06-23 15:40:02 -0300

    fix - error menssage when new user is created

--- src/middleware/validations.js
+++ src/middleware/validations.js
@@ -1,8 +1,6 @@
 const Joi = require('joi');
 const statusCode = require('../utils/httpStatus');
 
-// const MESSAGE = 'Some required fields are missing';
-
 const messageOnScreen = () => 'Some required fields are missing';
 
 const newUserValidation = (req, _res, next) => {
@@ -18,7 +16,7 @@ const newUserValidation = (req, _res, next) => {
   if (error) {
     return next({
       statusCode: statusCode.BAD_REQUEST,
-      message: messageOnScreen(),
+      message: error.details[0].message,
     });
   }
   next();
@@ -45,12 +43,7 @@ const newPost = (req, _res, next) => {
     title: Joi.string().required(),
     content: Joi.string().required(),
     categoryIds: Joi.array().items().min(1).required(),
-  });/* .messages({
-    'any.required': MESSAGE,
-    'string.empty': MESSAGE,
-    'array.base': MESSAGE,
-    'array.min': MESSAGE,
-  }) */
+  });
 
   const { error } = post.validate(req.body);
 
@@ -67,10 +60,7 @@ const updatePost = (req, _res, next) => {
   const post = Joi.object({
     title: Joi.string().required(),
     content: Joi.string().required(),
-  });/* .messages({
-    'any.required': MESSAGE,
-    'string.empty': MESSAGE,
-  }); */
+  });
 
   const { error } = post.validate(req.body);
 
